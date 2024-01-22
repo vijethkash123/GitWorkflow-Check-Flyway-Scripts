f7b7fdbead88262d3337ed09bd799695df84873a
vijethkash123 <vijethkashyap035@gmail.com>
2024-01-22 15:21:14 +0530

    update script

--- check_db_scripts.py
+++ check_db_scripts.py
@@ -29,56 +29,51 @@ if response.status_code != 200:
     print(f"Error: Unable to fetch changed files. Status code: {response.status_code}")
     exit(1)
 
-# changed_files_data = response.json()
-# file_names = [file['filename'] for file in changed_files_data]
-# print("Changed Files:")
-# for file_name in file_names:
-#     print(file_name)
+changed_files_data = response.json()
+file_names = [file['filename'] for file in changed_files_data]
+print("Changed Files:")
+for file_name in file_names:
+    print(file_name)
 
-# found = any('database_scripts' in file_name for file_name in file_names)
+found = any('database_scripts' in file_name for file_name in file_names)
 
-# print(found)
+print(found)
 
-# if not found:
-#     print("No need to run compare, No database scripts changed")
-#     exit(0)
+if not found:
+    print("No need to run compare, No database scripts changed")
+    exit(0)
 
-# else:
-#     for file in file_names:
-#         if 'database_scripts' in file:
-#             temp = file.split('/')
-#             versions_local.add(temp[2][:2])
+else:
+    for file in file_names:
+        if 'database_scripts' in file:
+            temp = file.split('/')
+            versions_local.add(temp[2][:2])
 
-# versions_local = sorted(versions_local)
-# # main_branch_url = fetch(
-# #         f"https://api.github.com/repos/vijethkash123/GitWorkflowTest/pulls/1",
-# #         headers={"accept": "application/vnd.github.v3, application/vnd.github.diff", "Authorization": f"token {READ_TOKEN}"},
-# #     )
-# # print(main_branch_url.content)
-# #
-# for env in ENVs:
-#     main_branch_url = requests.get(
-#         f"https://api.github.com/repos/vijethkash123/GitWorkflowTest/contents/database_scripts/{env}?ref=main",
-#         headers={"accept": "application/vnd.github.v3", "Authorization": f"token {READ_TOKEN}"},
-#     )
-#     if main_branch_url.status_code == 200:
-#         main_branch_data = json.loads(main_branch_url.content)
-#         for file_info in main_branch_data:
-#             versions_main.add(file_info['name'][:2])
-#     else:
-#         print("Unable to get the version of main branch")
-#         print(f"Got status code : {main_branch_url.status_code}")
-#         exit(1)
+versions_local = sorted(versions_local)
+
+for env in ENVs:
+    main_branch_url = requests.get(
+        f"https://api.github.com/repos/vijethkash123/GitWorkflowTest/contents/database_scripts/{env}?ref=main",
+        headers={"accept": "application/vnd.github.v3", "Authorization": f"token {READ_TOKEN}"},
+    )
+    if main_branch_url.status_code == 200:
+        main_branch_data = json.loads(main_branch_url.content)
+        for file_info in main_branch_data:
+            versions_main.add(file_info['name'][:2])
+    else:
+        print("Unable to get the version of main branch")
+        print(f"Got status code : {main_branch_url.status_code}")
+        exit(1)
 
 
-# for item in versions_local:
-#     if item in versions_main:
-#         raise Exception(f"Version of {item} is already present in Main branch")
+for item in versions_local:
+    if item in versions_main:
+        raise Exception(f"Version of {item} is already present in Main branch")
 
-# print("Success")
-# exit(0)
-#
-#
+print("Success")
+exit(0)
+
+
 # for env in ENVs:
 #     local_branch_url = fetch(
 #         f"https://api.github.com/repos/vijethkash123/GitWorkflowTest/contents/database_scripts/{env}?ref={CURRENT_BRANCH}",
@@ -97,3 +92,11 @@ if response.status_code != 200:
 # print(sorted(versions_local))
 #
 # # result = check_version(main_branch_version, local_branch_version)
+
+
+# main_branch_url = fetch(
+#         f"https://api.github.com/repos/vijethkash123/GitWorkflowTest/pulls/1",
+#         headers={"accept": "application/vnd.github.v3, application/vnd.github.diff", "Authorization": f"token {READ_TOKEN}"},
+#     )
+# print(main_branch_url.content)
+#
